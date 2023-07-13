2974513ca233a5ccc01e014e6d0ce647d237befa
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-07-13 15:15:07 +0900

    update : 산타 애니메이션 추가

--- interative-page-by-scroll/js/main/ImgScroll.js
+++ interative-page-by-scroll/js/main/ImgScroll.js
@@ -1,6 +1,10 @@
 import { InterfaceScrollEvent } from "./InterfaceScrollEvent.js";
 
 export class ImgScroll extends InterfaceScrollEvent {
+  translateX = 160;
+  translateY = -26;
+  rotate = 23;
+
   constructor() {
     super();
     this.img = document.querySelector(".scrolling .santa img");
@@ -10,18 +14,32 @@ export class ImgScroll extends InterfaceScrollEvent {
     const info = super.getScrollInfo(this.img);
     const startY = info.startY + 100;
     const endY = info.endY + 100;
-    const currentScroll = window.scrollY + window.innerHeight;
-    if (!super.isArea({ startY, endY, currentScroll })) {
+    const currentScroll = window.scrollY + window.innerHeight - 100;
+    if (super.isArea({ startY, endY, currentScroll })) {
+      this.show({ startY, endY, currentScroll });
       return;
     }
-    this.show({ startY, endY, currentScroll });
+
+    if (endY < currentScroll) {
+      this.hide({ endY, currentScroll });
+    }
   }
 
   show({ startY, endY, currentScroll }) {
     const ratio = (currentScroll - startY) / (endY - startY);
-    const x = 160 - 160 * ratio;
-    const y = -(26 - 26 * ratio);
-    const rotate = 23 - 23 * ratio;
-    this.img.style.transform = `translate(${x}px, ${y}px) rotate(${rotate}deg)`;
+    const x = this.translateX - this.translateX * ratio;
+    const y = -(this.translateY - this.translateY * ratio);
+    const r = this.rotate - this.rotate * ratio;
+    this.img.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
+  }
+
+  hide({ endY, currentScroll }) {
+    const ratio = (currentScroll - endY) / 300;
+    if (ratio > 1) return;
+    const x = this.translateX * ratio;
+    const y = -(this.translateY * ratio);
+    const r = this.rotate * ratio;
+
+    this.img.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
   }
 }
